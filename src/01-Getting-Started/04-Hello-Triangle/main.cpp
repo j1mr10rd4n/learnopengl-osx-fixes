3a1996a77b4c459e749cca3c8613f0aa65033d62
Jim Riordan <jim@j1mr10rd4n.info>
2015-06-26 14:39:40 +1000

    Add support for OpenGL3+
    See https://github.com/j1mr10rd4n/learnopengl-osx-fixes/commit/e963aa16995fc21157f3782a76ceda954853acc3

--- src/01-Getting-Started/04-Hello-Triangle/main.cpp
+++ src/01-Getting-Started/04-Hello-Triangle/main.cpp
@@ -37,6 +37,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
